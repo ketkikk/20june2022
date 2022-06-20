18088e64c18ab9568d1cf8c6de67d2f66c5abc2d
ketkikk <kulketki7@gmail.com>
2022-06-20 21:03:03 +0530

    first commit

---
+++ var and dynamic keyword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace var_and_dynamic_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+            //x = "KETKI";
+
+            var y = "KETKI";
+            //y = 1;
+            dynamic x1 = 2;
+            x1 = "KETKI";
+            Console.WriteLine(x);
+            Console.WriteLine(x1);
+        }
+    }
+}
